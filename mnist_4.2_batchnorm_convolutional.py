977c12d2bfbf841ff588ddc7bcddef83d5862ff1
fbchow <fannybchow@gmail.com>
2018-05-16 14:02:49 -0400

    add videos

--- mnist_4.2_batchnorm_convolutional.py
+++ mnist_4.2_batchnorm_convolutional.py
@@ -21,7 +21,7 @@ print("Tensorflow version " + tf.__version__)
 tf.set_random_seed(0.0)
 
 # Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
-mnist = mnist_data.read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
+mnist = mnist_data.read_data_sets("data/fashion", one_hot=True, reshape=False, validation_size=0)
 
 # neural network structure for this sample:
 #
